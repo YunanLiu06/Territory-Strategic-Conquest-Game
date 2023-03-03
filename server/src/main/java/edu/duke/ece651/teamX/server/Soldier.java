243b976c927de0a8f10195183801b8985415c80b
cc804 <cc804@duke.edu>
2023-03-03 12:57:07 -0500

    basic unit

--- server/src/main/java/edu/duke/ece651/teamX/server/Soldier.java
+++ server/src/main/java/edu/duke/ece651/teamX/server/Soldier.java
@@ -3,5 +3,16 @@ package edu.duke.ece651.teamX.server;
 import java.util.ArrayList;
 
 public class Soldier extends Unit {
+    /**
+     * constructor name and amount
+     * @param name
+     * @param amount
+     */
+    public Soldier(String name, int amount) {
+        super(name, amount);
+    }
 
+
+
+    
 }
--- server/src/main/java/edu/duke/ece651/teamX/server/Unit.java
+++ server/src/main/java/edu/duke/ece651/teamX/server/Unit.java
@@ -1,5 +1,16 @@
 package edu.duke.ece651.teamX.server;
 
 public abstract class Unit {
-    
+    private String name;
+    private int amount;
+    /**
+     * constructor name and amount
+     * @param name
+     * @param amount
+     */
+    public Unit(String name, int amount){
+
+        this.name = name;
+        this.amount = amount;
+    }
 }
