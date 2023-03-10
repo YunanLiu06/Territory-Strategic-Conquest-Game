932c67ac3df731aa933890998770e363a17016b5
Xian McElrath <xian.mcelrath@duke.edu>
2023-03-10 14:54:49 -0500

    Mutliple clients connecting and able to distinguish which player is who

--- client/src/main/java/edu/duke/ece651/teamX/client/ClientIO.java
+++ client/src/main/java/edu/duke/ece651/teamX/client/ClientIO.java
@@ -4,38 +4,45 @@ import java.io.*;
 import java.net.*;
 import edu.duke.ece651.teamX.shared.*;
 
+/**
+ * This class is supposed to represent the client of the game
+ * 
+ * @param InputStream:  to take in input from server
+ * @param OutputStream: to write output to the client
+ */
 public class ClientIO {
 
   private InputStream in;
   private OutputStream out;
-  private ObjectOutputStream writeObject;
-  private ObjectInputStream readObject;
+  private DataOutputStream writeObject;
+  private DataInputStream readObject;
 
   static String CONSTRUCTOR_ERROR = "Constructor Error: ";
 
   static String IO_ERROR = "IO Error: ";
 
   public ClientIO(InputStream in, OutputStream out) {
-    try {
-      this.in = in;
-      this.out = out;
-      writeObject = new ObjectOutputStream(out);
-      readObject = new ObjectInputStream(in);
-    } catch (IOException e) {
-      System.out.println(CONSTRUCTOR_ERROR + e + "\n");
-    }
-
+    this.in = in;
+    this.out = out;
+    writeObject = new DataOutputStream(out);
+    readObject = new DataInputStream(in);
   }
 
+  /**
+   * This function is to grab the information from the server
+   * The name of the player and the map
+   */
   public void initalizationPhase() {
     try {
 
-      // print the map from the Server
-      String stringFromServer = (String) readObject.readObject();
-      System.out.println(stringFromServer);
+      // print the welcome message of the game
+      System.out.println(readObject.readUTF());
+      // print the player name
+      System.out.println(readObject.readUTF());
+      // print the map of the game
+      System.out.println(readObject.readUTF());
+
     } catch (IOException e) {
-      System.out.println(IO_ERROR + e + "\n");
-    } catch (ClassNotFoundException e) {
       System.out.println(IO_ERROR + e + "\n");
     }
   }
--- server/src/main/java/edu/duke/ece651/teamX/server/ServerIO.java
+++ server/src/main/java/edu/duke/ece651/teamX/server/ServerIO.java
@@ -5,40 +5,50 @@ import java.net.*;
 import java.io.*;
 import java.util.LinkedList;
 
+import javax.xml.crypto.Data;
+
 /**
  * This class handles the input and output for the Risc game.
+ * 
+ * @param socket: the socket of the client
+ * @param in:     the input stream of this specfic socket
+ * @param out:    the output stram of this specific socket
+ * @param name:   the name of the player
+ * @param map:    the map of the game
  */
-public class ServerIO {
-
+public class ServerIO extends Thread {
+  private Socket socket;
   private InputStream in;
   private OutputStream out;
-  private ObjectOutputStream writeObject;
-  private ObjectInputStream readObject;
+  private DataOutputStream writeObject;
+  private DataInputStream readObject;
+  private String name;
+  private GameMap gameMap;
+  private Player player;
 
- 
   // string to state that there was an IOException in ther server
   static String CONSTRUCTOR_ERROR = "Constructor Error: ";
 
   // strign to state that there was an IOError
   static String IO_ERROR = "IO Error: ";
 
-  public ServerIO(InputStream in, OutputStream out) {
-    try {
-      this.in = in;
-      this.out = out;
-      writeObject = new ObjectOutputStream(out);
-      readObject = new ObjectInputStream(in);
-    } catch (IOException e) {
-      System.out.println(CONSTRUCTOR_ERROR + e + "\n");
-    }
+  public ServerIO(Socket socket, InputStream in, OutputStream out, String name, GameMap gameMap) {
+    // initialize IO
+    this.socket = socket;
+    this.in = in;
+    this.out = out;
+    writeObject = new DataOutputStream(out);
+    readObject = new DataInputStream(in);
+    // intialize other things for the game
+    this.name = name;
+    this.gameMap = gameMap;
+    player = new TextPlayer(name);
   }
 
   /**
    * Private helper function to return a string of the text map
    */
   private String printTextMap() {
-    MapGenerator mapGenerator = new FixMapGenerator();
-    GameMap gameMap = mapGenerator.createMap(2);
     MapView<String> mapView = new TextMapView();
     String view = mapView.printMap(gameMap);
     return view;
@@ -46,18 +56,24 @@ public class ServerIO {
 
   /**
    * This function is the initialization phase of the game
-   It welcomes the users to the game, gets how many players are in the game,
-   
+   * It welcomes the users to the game, and prints the map
+   * to the players
    */
   public void initializationPhase() {
     try {
-
+      writeObject.writeUTF("\nWelcome to the Game of Risc.");
+      writeObject.writeUTF("Your Player Name: " + player.getName() + "\n" + "Here is the map of the game\n");
       // generate Map and send to Client so they can print and exit
       String view = printTextMap();
-      writeObject.writeObject(view);
+      writeObject.writeUTF(view);
 
     } catch (IOException e) {
       System.out.println(IO_ERROR + e + "\n");
     }
+  }
+
+  @Override
+  public void run() {
+    initializationPhase();
   }
 }
