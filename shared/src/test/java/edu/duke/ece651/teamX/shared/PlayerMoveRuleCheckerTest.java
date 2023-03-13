526041da66ab7f14d1b92202385c88aeeb671479
Yunan Liu <yl852@duke.edu>
2023-03-12 23:04:17 -0600

    update PlayerMoveRuleChecker and corresponding tests

--- shared/src/test/java/edu/duke/ece651/teamX/shared/PlayerMoveRuleCheckerTest.java
+++ shared/src/test/java/edu/duke/ece651/teamX/shared/PlayerMoveRuleCheckerTest.java
@@ -21,9 +21,10 @@ public class PlayerMoveRuleCheckerTest {
 
   @Test
   public void test_checkRule() {
-    Territory t1 = new Territory("TestTerritory1");
-    Territory t2 = new Territory("TestTerritory2");
-    Territory t3 = new Territory("TestTerritory3");
+    Player p1 = new TextPlayer("p1");
+    Territory t1 = new Territory("TestTerritory1", p1);
+    Territory t2 = new Territory("TestTerritory2", p1);
+    Territory t3 = new Territory("TestTerritory3", p1);
     Soldier s1 = new Soldier( 10);
     Soldier s2 = new Soldier( 20);
     Soldier s3 = new Soldier( 3);
@@ -35,16 +36,39 @@ public class PlayerMoveRuleCheckerTest {
     assertEquals(playerMoveRuleChecker.checkRule(t1, t2, 5), true);
     assertEquals(playerMoveRuleChecker.checkRule(t1, t2, 15), false);
     assertEquals(playerMoveRuleChecker.checkRule(t1, t3, 5), false);
-    Territory t4 = new Territory("TestTerritory4");
+    Territory t4 = new Territory("TestTerritory4", p1);
     Soldier s4 = new Soldier( 10);
-    Territory t5 = new Territory("TestTerritory5");
+    t4.addUnit(s4);
+    Territory t5 = new Territory("TestTerritory5", p1);
     Soldier s5 = new Soldier( 10);
-    Territory t6 = new Territory("TestTerritory6");
+    t5.addUnit(s5);
+    Territory t6 = new Territory("TestTerritory6", p1);
     Soldier s6 = new Soldier( 10);
+    t6.addUnit(s6);
     t2.addAdjacentTerritory(t4);
     t1.addAdjacentTerritory(t5);
     t5.addAdjacentTerritory(t6);
     assertEquals(playerMoveRuleChecker.checkRule(t1, t6, 5), true);
+    t4 = new Territory("TestTerritory4", p1);
+    t4.addUnit(s4);
+    t5 = new Territory("TestTerritory5", p1);
+    t5.addUnit(s5);
+    t6 = new Territory("TestTerritory6");
+    t6.addUnit(s6);
+    t2.addAdjacentTerritory(t4);
+    t1.addAdjacentTerritory(t5);
+    t5.addAdjacentTerritory(t6);
+    assertEquals(playerMoveRuleChecker.checkRule(t1, t6, 5), false);
+    Player p2 = new TextPlayer("p2");
+    Territory t7 = new Territory("TestTerritory1", p2);
+    Territory t8 = new Territory("TestTerritory2");
+    Territory t9 = new Territory("TestTerritory3", p2);
+    t7.addUnit(s1);
+    t8.addUnit(s2);
+    t9.addUnit(s3);
+    t7.addAdjacentTerritory(t8);
+    t8.addAdjacentTerritory(t9);
+    assertEquals(playerMoveRuleChecker.checkRule(t7, t9, 1), false);
   }
 
 }
