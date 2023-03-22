bf291b06150709500c0822b9078bbb3f573947de
Xian McElrath <xian.mcelrath@duke.edu>
2023-03-22 19:40:10 -0400

    More test coverage in server

--- server/src/test/java/edu/duke/ece651/teamX/server/AppTest.java
+++ server/src/test/java/edu/duke/ece651/teamX/server/AppTest.java
@@ -3,13 +3,39 @@ package edu.duke.ece651.teamX.server;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class AppTest {
-  /*
-   * @Test
-   * void test_GetMessage() {
-   * App a = new App();
-   * assertEquals("Hello from the server for teamX", a.getMessage());
-   * }
-   */
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.parallel.ResourceAccessMode;
+import org.junit.jupiter.api.parallel.ResourceLock;
+import org.junit.jupiter.api.parallel.Resources;
+
+@Test
+@ResourceLock(value = Resources.SYSTEM_OUT, mode = ResourceAccessMode.READ_WRITE)
+class AppTest throws IOException {
+  ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+  PrintStream out = new PrintStream(bytes, true);
+  InputStream input = getClass().getClassLoader().getResourceAsStream("input.txt");
+  assertNotNull(input);
+  InputStream expectedStream = getClass().getClassLoader().getResourceAsStream("output.txt");
+  assertNotNull(expectedStream);
+  InputStream oldIn = System.in;
+  PrintStream oldOut = System.out;
+  try {
+     System.setIn(input);
+     System.setOut(out);
+     App.main(new String[0]);
+  }
+  finally {
+    System.setIn(oldIn);
+    System.setOut(oldOut);
+  }
+  String expected = new String(expectedStream.readAllBytes());
+  String actual = bytes.toString();
+  assertEquals(expected, actual);
 }
